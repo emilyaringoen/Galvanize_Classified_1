1934dff73a994bb4df822475ac769f35ff438c79
Emily Ringoen <emilyaringoen@gmail.com>
2017-05-17 10:52:09 -0600

    finished part III

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -8,6 +8,7 @@ const knex = require('../knex')
 
 router.get('/', (req, res, next) => {
   knex('classifieds')
+    .select('id', 'title', 'description', 'price', 'item_image')
     .then((items) => {
       res.send(items)
     })
@@ -15,54 +16,57 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
   let id = Number.parseInt(req.params.id)
-  return knex('classifieds')
+  knex('classifieds')
+    .select('id', 'title', 'description', 'price', 'item_image')
     .where('id', id)
-    .then((book) => {
-      res.set('Content-Type', 'application/json')
-      res.send(humps.camelizeKeys(book[0]))
+    .then((item) => {
+      res.send(item[0])
     })
 })
 
 
 router.post('/', (req, res, next) => {
   knex('classifieds')
-    .returning('*')
+    .returning(['id', 'title', 'description', 'price', 'item_image'])
     .insert({
       title: req.body.title,
-      author: req.body.description,
-      genre: req.body.price,
-      description: req.body.item_image
+      description: req.body.description,
+      price: req.body.price,
+      item_image: req.body.item_image
     }).then((item) => {
-      res.send(item)
+      res.send(item[0])
     })
 })
 
 router.patch('/:id', (req, res, next) => {
   let id = Number.parseInt(req.params.id)
   let updated = req.body
+
   knex('classifieds')
     .where('id', id)
-    .then((item) => {
-      item = humps.camelizeKeys(item[0])
-      for (const key in item) {
+    .returning(['id', 'title', 'description', 'price', 'item_image'])
+    .then((el) => {
+      let item = el[0]
+      for (let key in item) {
         if (updated.hasOwnProperty(key)) {
           item[key] = updated[key]
         }
       }
-      res.send(humps.camelizeKeys(item))
+      delete item.created_at
+      delete item.updated_at
+      res.set('Content-Type', 'application/json')
+      res.send(item)
     })
 })
 
 router.delete('/:id', (req, res, next) => {
   let id = Number.parseInt(req.params.id)
   knex('classifieds')
-    .returning(['title', 'author', 'genre', 'description', 'cover_url'])
+    .returning(['id', 'title', 'description', 'price', 'item_image'])
     .where('id', id)
     .del()
-    .then((book) => {
-      res.send(humps.camelizeKeys(book[0]))
-    }).catch((err) => {
-      next(err)
+    .then((item) => {
+      res.send(item[0])
     })
 })
 
