c3f88564d82c9e85afc85757371a20f383ac9805
Emily Ringoen <emilyaringoen@gmail.com>
2017-05-17 10:28:25 -0600

    part I & II finished

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -2,9 +2,70 @@
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
+const knex = require('../knex')
 
-// YOUR CODE HERE
+
+router.get('/', (req, res, next) => {
+  knex('classifieds')
+    .then((items) => {
+      res.send(items)
+    })
+})
+
+router.get('/:id', (req, res, next) => {
+  let id = Number.parseInt(req.params.id)
+  return knex('classifieds')
+    .where('id', id)
+    .then((book) => {
+      res.set('Content-Type', 'application/json')
+      res.send(humps.camelizeKeys(book[0]))
+    })
+})
+
+
+router.post('/', (req, res, next) => {
+  knex('classifieds')
+    .returning('*')
+    .insert({
+      title: req.body.title,
+      author: req.body.description,
+      genre: req.body.price,
+      description: req.body.item_image
+    }).then((item) => {
+      res.send(item)
+    })
+})
+
+router.patch('/:id', (req, res, next) => {
+  let id = Number.parseInt(req.params.id)
+  let updated = req.body
+  knex('classifieds')
+    .where('id', id)
+    .then((item) => {
+      item = humps.camelizeKeys(item[0])
+      for (const key in item) {
+        if (updated.hasOwnProperty(key)) {
+          item[key] = updated[key]
+        }
+      }
+      res.send(humps.camelizeKeys(item))
+    })
+})
+
+router.delete('/:id', (req, res, next) => {
+  let id = Number.parseInt(req.params.id)
+  knex('classifieds')
+    .returning(['title', 'author', 'genre', 'description', 'cover_url'])
+    .where('id', id)
+    .del()
+    .then((book) => {
+      res.send(humps.camelizeKeys(book[0]))
+    }).catch((err) => {
+      next(err)
+    })
+})
+
+
 
 module.exports = router;
